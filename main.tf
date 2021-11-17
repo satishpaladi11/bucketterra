066c597427c695515a6aed66c57185886b89f0a7
SamSathish <satishpaladi11@gmail.com>
2021-11-17 01:24:54 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.1.0"
-    }
-  }
-}
 provider "google" {
   project     = "calcium-post-282611"
   region      = "us-central1"
