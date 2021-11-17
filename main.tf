921c03ebbc5636450dcc468e18c8b7f8af80e4e5
SamSathish <satishpaladi11@gmail.com>
2021-11-17 01:24:02 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "google" {
   project     = "calcium-post-282611"
   region      = "us-central1"
 }
-resource "google_storage_bucket" {
+resource "google_storage_bucket" "auto-expire"{
   name          = "terrabucket85558"
   location      = "US"
 
