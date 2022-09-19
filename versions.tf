9bf2ee2199e44a5e0bcaef02355e337095e4f65d
Paladi Sathish <m1066781@mindtree.com>
2022-09-19 23:46:39 +0530

    chore:add provider

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
-terraform {
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.36.0"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
+#terraform {
+#
+#  required_providers {
+#    google = {
+#      source  = "hashicorp/google"
+#      version = "~> 4.36.0"
+#    }
+#  }
+#
+#  required_version = ">= 1.1.0"
+#}
